24b325dbe7be5faadb8f3755a0bc019e07cd6973
Yu Wei <ywstar89@gmail.com>
2016-02-21 12:28:42 +0800

    Use pyenv to install 3.4.

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -20,38 +20,26 @@ sudo apt-get update -y
 sudo apt-get install -y build-essential
 sudo apt-get install -y libc6-dev libreadline-dev libz-dev libncursesw5-dev libssl-dev libgdbm-dev libsqlite3-dev libbz2-dev liblzma-dev tk-dev
 
-sudo apt-get install -y git-core mercurial vim screen wget curl raptor-utils unzip ack-grep
-sudo apt-get install -y tree vim-gnome
+sudo apt-get install -y git-core mercurial screen wget curl raptor-utils unzip ack-grep
+sudo apt-get install -y tree
 
 #apache, xvfb and firefox for selenium functional testing
 sudo apt-get install -y apache2 xvfb firefox
 
 sudo apt-get purge python-pip
 sudo apt-get install -y python-pip
-sudo pip install virtualenv virtualenvwrapper
 sudo apt-get update -y
 
 ## end main server installs ##
 
-## virtualenv work ##
-
-sudo echo """
-export WORKON_HOME=~/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-""" >> /home/vagrant/.bashrc
-source /home/vagrant/.bashrc
-
 #Install Python 3.4 for to run in a virtual environment
-cd /home/vagrant/
-sudo wget https://www.python.org/ftp/python/3.4.1/Python-3.4.1.tar.xz
-sudo tar -xvf Python-3.4.1.tar.xz
-cd Python-3.4.1
-sudo ./configure
-make && sudo make install
-sudo rm -rf Python-3.4.1*
-
-#Create a virtualenv and ACTIVATE it
-sudo pyvenv-3.4 /home/vagrant/.virtualenvs/py341
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+exec $SHELL
+pyenv install 3.4
+pyenv rehash
 
 #finished with virtualenv work
 
